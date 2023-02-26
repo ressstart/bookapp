b9c92c2a580509ac405d854341a0c64fec92ab91
ressstart <lressstart@gmail.com>
2023-02-26 18:29:24 +0300

    delete Activity

--- app/src/main/java/com/ressstart/bookapp/UpdateActivity.java
+++ app/src/main/java/com/ressstart/bookapp/UpdateActivity.java
@@ -1,7 +1,10 @@
 package com.ressstart.bookapp;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +14,7 @@ import android.widget.Toast;
 public class UpdateActivity extends AppCompatActivity {
 
     EditText title_input, author_input, pages_input;
-    Button update_button;
+    Button update_button, delete_button;
 
     String id, title, author, pages;
 
@@ -24,8 +27,15 @@ public class UpdateActivity extends AppCompatActivity {
         author_input = findViewById(R.id.author_input2);
         pages_input = findViewById(R.id.pages_input2);
         update_button = findViewById(R.id.update_button);
+        delete_button = findViewById(R.id.delete_button);
 
         getAndSetIntentData();
+
+        ActionBar ab = getSupportActionBar();
+        if (ab != null){
+            ab.setTitle(title);
+        }
+
 
         update_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,7 +48,12 @@ public class UpdateActivity extends AppCompatActivity {
             }
         });
         //getAndSetIntentData();
-
+        delete_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                confirmDialog();
+            }
+        });
     }
 
     void getAndSetIntentData(){
@@ -57,5 +72,26 @@ public class UpdateActivity extends AppCompatActivity {
         else{
             Toast.makeText(this, "No data", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    void confirmDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Delete " + title + "?");
+        builder.setMessage("R u sure to delete " + title + "?");
+        builder.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                MyDatabaseHelper myDB = new MyDatabaseHelper(UpdateActivity.this);
+                myDB.deleteOneRow(id);
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        builder.create().show();
     }
 }
