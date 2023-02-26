4307705c6a272d238cbea995df0e7883305ef158
ressstart <lressstart@gmail.com>
2023-02-26 18:00:24 +0300

    update Activity

--- app/src/main/java/com/ressstart/bookapp/CustomAdapter.java
+++ app/src/main/java/com/ressstart/bookapp/CustomAdapter.java
@@ -1,23 +1,30 @@
 package com.ressstart.bookapp;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
 
     private Context context;
+    Activity activity;
     private ArrayList book_id, book_title, book_author, book_pages;
-    CustomAdapter(Context context, ArrayList book_id, ArrayList book_title, ArrayList book_author,
+    //int position;
+    CustomAdapter(Activity activity, Context context, ArrayList book_id, ArrayList book_title, ArrayList book_author,
                   ArrayList book_pages){
+        this.activity = activity;
         this.context = context;
         this.book_id = book_id;
         this.book_title = book_title;
@@ -34,10 +41,23 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        //this.position = position;
+
         holder.book_id_txt.setText(String.valueOf(book_id.get(position)));
         holder.book_title_txt.setText(String.valueOf(book_title.get(position)));
         holder.book_author_txt.setText(String.valueOf(book_author.get(position)));
         holder.book_pages_txt.setText(String.valueOf(book_pages.get(position)));
+        holder.mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, UpdateActivity.class);
+                intent.putExtra("id", String.valueOf(book_id.get(position)));
+                intent.putExtra("title", String.valueOf(book_title.get(position)));
+                intent.putExtra("author", String.valueOf(book_author.get(position)));
+                intent.putExtra("pages", String.valueOf(book_pages.get(position)));
+                activity.startActivityForResult(intent, 1);
+            }
+        });
     }
 
     @Override
@@ -48,6 +68,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
         TextView book_id_txt, book_title_txt, book_author_txt, book_pages_txt;
+        LinearLayout mainLayout;
         public MyViewHolder(@NonNull View itemView) {
 
             super(itemView);
@@ -55,6 +76,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
             book_title_txt = itemView.findViewById(R.id.book_title_txt);
             book_author_txt = itemView.findViewById(R.id.book_author_txt);
             book_pages_txt = itemView.findViewById(R.id.book_pages_txt);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
         }
     }
 }
